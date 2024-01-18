908469834a036b6c19d2d7faefd207bcb6cbbee7
dongil <dikwon79@naver.com>
2024-01-18 15:57:38 +0900

    setting first

---
+++ lang/messages/en/user.js
@@ -0,0 +1,12 @@
+//string messages
+const messages = {
+    error: {
+      correctOrder: 'Correct orders',
+      incorrectOrder: 'Wrong orders!',
+      excellent: 'Excellent memory!',
+      condition: 'input error(3~7)',
+    },
+    gameover: "Game Over",
+};
+
+  
